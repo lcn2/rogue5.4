ae3dada0bea0598bf01ffe1ace9eb966126afb0d
Landon Curt Noll <github-mail@asthe.com>
2026-02-15 16:15:07 -0800

    update version to (github.com/lcn2/rogue5.4) 2026-02-15
    Changed `version[]` array to reflect significant
    changes from the previous commit.

--- vers.c
+++ vers.c
@@ -30,4 +30,4 @@
 const char *release = "5.4.5";
 const char encstr[] = "\300k||`\251Y.'\305\321\201+\277~r\"]\240_\223=1\341)\222\212\241t;\t$\270\314/<#\201\254";
 const char statlist[] = "\355kl{+\204\255\313idJ\361\214=4:\311\271\341wK<\312\321\213,,7\271/Rk%\b\312\f\246";
-const char version[] = "(github.com/lcn2/rogue5.4) 2026-02-11";
+const char version[] = "(github.com/lcn2/rogue5.4) 2026-02-15";
