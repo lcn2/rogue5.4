lcn2/rogue5.4

commit 40f795c7a5da48afb8e0a826cbff78b8f8ec35b1

release 2026-03-22
Correctly document the new "**v**" command in the help message.

Added "**;**" command as a no-op, no time passing command.
The **;**" command is often used by rogomatic.

Explain in help that "~^[~" is the escape key.

Update `README.md` accordingly.

Changed release date to "2026-03-22".

command.c
@@ -374,6 +374,9 @@ over:
 		    after = false;
 		when CTRL('Z'):
 		    tstp(0);
+		when ';':
+		    /* The ; command is ignored, no time passes - often used by rogomatic */
+		    after = false;
 		otherwise:
 		    after = false;
 #ifdef MASTER

extern.c
@@ -399,12 +399,13 @@ const struct h_list helpstr[] = {
     {'o',	"	examine/set options",			true},
     {CTRL('R'),	"	redraw screen",				true},
     {CTRL('P'),	"	repeat last message",			true},
-    {ESCAPE,	"	cancel command",			true},
+    {ESCAPE,	"	cancel command, ^[ is the escape key",	true},
     {'S',	"	save game",				true},
     {'Q',	"	quit",					true},
     {'!',	"	shell escape",				true},
     {'F',	"<dir>	fight till either of you dies",		true},
-    {'v',	"	print version and release",		true},
+    {'v',	"	print version, release, dungeon number",true},
+    {';',	"	command is ignored, no time passes",	true},
     {0,		NULL }
 };
 int numscores = NUMSCORES;

vers.c
@@ -8,7 +8,7 @@
  *
  * IMPORTANT NOTE: As of rogue 5.4.5 2026-02-18, the version, but NOT the release is written to the rogue save file.
  *		   So when you change the version string, you invalidate all old rogue save files!
- *		   However, you may now change the release without impacting rogue save files.
+ *		   However, you may now change the release date without impacting rogue save files.
  *
  * NOTE: The encstr[] and statlist[] are used by encwrite() and encread().
  *	 You probably don't want to change either of them as doing so would almost
@@ -24,4 +24,4 @@
 const char version[] = "5.4.5";
 const char encstr[] = "\300k||`\251Y.'\305\321\201+\277~r\"]\240_\223=1\341)\222\212\241t;\t$\270\314/<#\201\254";
 const char statlist[] = "\355kl{+\204\255\313idJ\361\214=4:\311\271\341wK<\312\321\213,,7\271/Rk%\b\312\f\246";
-const char release[] = "2026-03-18";
+const char release[] = "2026-03-22";

commit 63286892dadb68f70ab89c38b8520a2f35a31361

release 2026-03-23
The change in commit 40f795c7a5da48afb8e0a826cbff78b8f8ec35b1
broke rogomatic in a non-obvious way.  We revert "**;**" command
to be an illegal command.

Changed release date to "2026-03-23".

command.c
@@ -374,9 +374,6 @@ over:
 		    after = false;
 		when CTRL('Z'):
 		    tstp(0);
-		when ';':
-		    /* The ; command is ignored, no time passes - often used by rogomatic */
-		    after = false;
 		otherwise:
 		    after = false;
 #ifdef MASTER

extern.c
@@ -405,7 +405,6 @@ const struct h_list helpstr[] = {
     {'!',	"	shell escape",				true},
     {'F',	"<dir>	fight till either of you dies",		true},
     {'v',	"	print version, release, dungeon number",true},
-    {';',	"	command is ignored, no time passes",	true},
     {0,		NULL }
 };
 int numscores = NUMSCORES;

vers.c
@@ -24,4 +24,4 @@
 const char version[] = "5.4.5";
 const char encstr[] = "\300k||`\251Y.'\305\321\201+\277~r\"]\240_\223=1\341)\222\212\241t;\t$\270\314/<#\201\254";
 const char statlist[] = "\355kl{+\204\255\313idJ\361\214=4:\311\271\341wK<\312\321\213,,7\271/Rk%\b\312\f\246";
-const char release[] = "2026-03-22";
+const char release[] = "2026-03-23";